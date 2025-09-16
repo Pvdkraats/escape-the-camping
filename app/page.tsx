f4395bb9297be752f56af3f8f999d8b170aa1ca7
Patrick van de Kraats <P.kraats88@gmail.com>
2025-09-16 22:32:49 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/hero-section";
+import Uitleg from "@/components/uitleg";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Uitleg />
+    </>
+  );
+}
---
+++ components/uitleg.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Uitleg() {
+  return (
+    <div className='bg-gray-200 w-full h-6xl'>
+      <div className='container mx-auto px-4 py-16 max-w-5xl'>
+        <h2 className='text-2xl font-bold text-gray-700'>Wat is Escape the Camping?</h2>
+        <p className='mt-4 text-gray-600'>Een Escape the Camping game speelt zich af op de door jou geselecteerde camping. Aan de hand van onze app, die je op een eigen smartphone installeert, maken jullie een wandeling langs de leukste plekken op de camping. 
+          Samen met je gezin of team krijg je 2 uur de tijd om verschillende raadsels en puzzels op te lossen, aanwijzingen te ontcijferen en vragen goed te beantwoorden. </p>
+        <p className='mt-4 text-gray-600'>De game is geschikt voor alle leeftijden en kan gespeeld worden met groepen van 2 tot 6 personen. Het is een geweldige manier om quality time door te brengen met je gezin of vrienden, terwijl je samenwerkt om de uitdagingen te overwinnen.</p>
+        <h2 className='text-2xl font-bold text-gray-700 mt-8'>Hoe werkt het?</h2>
+        <p className='mt-4 text-gray-600'>Na het plaatsen van de bestelling ontvang je een bevestigingsmail met daarin alle benodigde informatie om te starten en een downloadlink voor de app. Ook ontvang je in de mail je inloggegevens.Er is geen instructeur ter plaatse en na het boeken kan je de game starten wanneer je wilt.</p>
+        <p className='mt-4 text-gray-600'>Na het inloggen op de app en het invoeren van uw teamnaam, kunt u de game starten. Let op dat de tijd begint te lopen zodra u de game gestart heeft, dus zorg ervoor dat je er klaar voor bent!</p>
+        <p className='mt-4 text-gray-600'>Voor het gebruik van de app is internet nodig. Probeer dus voor een goede internetverbinding en een volle accu te zorgen.</p>
+        <p className='mt-4 text-gray-600'>Het team met de snelste tijd verdient een plekje in de week- of overall ranking. Succes en veel plezier bij het ontsnappen van de camping!</p>
+      </div>
+    </div>
+  )
+}
